fcc2ff5323c2afa0436107abe1adee7db6b1f163
Juan Manuel Aidar <j.aidar@alumno.um.edu.ar>
2025-08-04 15:19:18 -0300

    Implementación de servicios y excepciones (Issues 3 y 4 de Etapa 2)

---
+++ gestion-empleados/src/main/java/ar/edu/um/gestion_empleados/exception/RecursoNoEncontradoException.java
@@ -0,0 +1,1 @@
+package ar.edu.um.gestion_empleados.exception; public class RecursoNoEncontradoException extends RuntimeException { public RecursoNoEncontradoException(String mensaje) { super(mensaje); } }
